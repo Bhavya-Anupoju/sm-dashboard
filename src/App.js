3bea8a443be7025b1130e1216261698789187ebb
Bhavya-Anupoju <67477724+Bhavya-Anupoju@users.noreply.github.com>
2024-08-26 20:35:40 +0530

    Theme Context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import { ThemeProvider } from './Components/Theme';
 import { Profile } from './Pages/Profile';
 
 function App() {
   return (
+    <ThemeProvider>
     <div className="App">
       <Profile />
     </div>  
+    </ThemeProvider>
   );
 }
 
--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { useTheme } from "../Components/Theme";
 
 export const Profile = () => {
+    const {theme, toggleTheme} = useTheme();
 
     const [formData, setFormData] = useState({
         name : '',
@@ -40,41 +42,49 @@ export const Profile = () => {
             [name]: value
         })
     }
-
-
-    return(
+    
+    return (
         <>
+            <form onSubmit={handleSubmit} className={`flex items-center flex-col justify-center h-screen ${theme === "light" ? "bg-white" : "bg-gray-800 text-white"}`}>
+                <h2 className={`text-3xl font-bold ${theme === "light" ? "text-blue-700" : "text-white"} mb-2`}>Profile</h2>
 
-        <form onSubmit={handleSubmit} className="flex items-center flex-col justify-center h-screen">
-        <h2 className="text-3xl font-bold text-blue-700">Profile</h2>
+                <div className={`p-8 rounded-lg shadow-md w-full max-w-md ${theme === "light" ? "bg-white" : "bg-gray-900"}`}>
+                    <div className="flex flex-col mb-4">
+                        <label htmlFor="name" className={`font-semibold ${theme === "light" ? "text-blue-700" : "text-white"}`}>
+                            Full Name<span className="text-red-500">*</span>
+                        </label>
+                        <input id="name" type="text" placeholder="Enter your Name" name="name" onChange={handleChange} value={formData.name} className={`focus:outline-none ${theme === "light" ? "bg-white" : "bg-gray-900 text-white"}`} />
+                        {errors.name && <p className="text-red-500 text-sm mt-1">{errors.name}</p>}
+                    </div>
 
-            <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+                    <div className="flex flex-col mb-4">
+                        <label htmlFor="phone-number" className={`font-semibold ${theme === "light" ? "text-blue-700" : "text-white"}`}>Phone Number</label>
+                        <input id="phone-number" type="Number" placeholder="Phone Number" name="phoneNumber" onChange={handleChange} value={formData.phoneNumber} className={`focus:outline-none ${theme === "light" ? "bg-white" : "bg-gray-900 text-white"}`} />
+                    </div>
 
-            <div className="flex flex-col mb-4">
-            <label htmlFor="name" className="text-blue-700 font-semibold">Full Name<span className="text-red-500">*</span></label>
-            <input id="name" type="text" placeholder="Enter your Name" name="name" onChange={handleChange} value={formData.name} className="focus:outline-none" />
-            {errors.name && <p className="text-red-500 text-sm mt-1">{errors.name}</p>}
-            </div>
-            <div className="flex flex-col mb-4">
-            <label htmlFor="phone-number" className="text-blue-700 font-semibold">Phone Number</label>
-            <input id="phone-number" type="Number" placeholder="Phone Number" name="phoneNumber"  onChange={handleChange} value={formData.phoneNumber} className="focus:outline-none"/>
-            </div>
+                    <div className="flex flex-col mb-4">
+                        <label htmlFor="email" className={`font-semibold ${theme === "light" ? "text-blue-700" : "text-white"}`}>
+                            Email<span className="text-red-500">*</span>
+                        </label>
+                        <input id="email" type="email" placeholder="Email" name="email" onChange={handleChange} value={formData.email} className={`focus:outline-none ${theme === "light" ? "bg-white" : "bg-gray-900 text-white"}`} />
+                        {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email}</p>}
+                    </div>
 
-            <div className="flex flex-col mb-4">
-            <label htmlFor="email" className="text-blue-700 font-semibold">Email<span className="text-red-500">*</span></label>
-            <input id="email" type="email" placeholder="Email" name="email" onChange={handleChange} value={formData.email} className="focus:outline-none"/>
-            {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email}</p>}
-            </div>
+                    <div className="flex flex-col mb-4">
+                        <label htmlFor="password" className={`font-semibold ${theme === "light" ? "text-blue-700" : "text-white"}`}>
+                            Password<span className="text-red-500">*</span>
+                        </label>
+                        <input id="password" type="password" placeholder="Password" name="password" onChange={handleChange} value={formData.password} className={`focus:outline-none ${theme === "light" ? "bg-white" : "bg-gray-900 text-white"}`} />
+                        {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password}</p>}
+                    </div>
 
-            <div className="flex flex-col mb-4">
-            <label htmlFor="password" className="text-blue-700 font-semibold" >Password<span className="text-red-500">*</span>   </label>
-            <input id="password" type="password" placeholder="Password" name="password" onChange={handleChange} value={formData.password} className="focus:outline-none"/>
-            {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password}</p>}
-            </div>
-
-            <button type="submit" className="bg-blue-700 text-white p-2 rounded-lg">Update</button>
-            </div>
-        </form>
+                    <button type="submit" className={`p-2 rounded-lg ${theme === "light" ? "bg-blue-700 text-white" : "bg-blue-500 text-white"}`}>Update</button>
+                    <button onClick={toggleTheme} className={`p-2 mx-2 rounded-lg ${theme === "light" ? "bg-blue-700 text-white" : "bg-blue-500 text-white"}`}>
+                Toggle to {theme === "light" ? "Dark" : "Light"} Theme
+            </button>
+                </div>
+            </form>
         </>
-    )
+    );
+
 }
